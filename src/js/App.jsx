89f47341a1fe5113c06a28497d437bc84bd892f4
molnard1 <101341428+molnard1@users.noreply.github.com>
2023-11-22 10:43:16 +0100

    I hate bootstrap

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,34 +1,42 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { Col, Container, Image, Row, Spinner } from "react-bootstrap";
 import { createRoot } from 'react-dom/client';
 
 export default function App() {
 	const [data, setData] = useState([]);
+	const [loaded, setLoaded] = useState(false);
 
 	useEffect(() => {
 		(async() => {
 			let res = await axios.get("https://pizza.kando-dev.eu/Pizza");
 			setData(res.data);
+			setLoaded(true);
 		})();
 	}, []);
 
 	let items = data.map(item => (
-		<div style={{
-			border: "2px solid black",
-			display: "inline-block",
-			marginRight: "20px",
-		}} key={item.id}>
-			<h1 style={{
-				margin: "0px"
-			}}>{item.name}</h1>
-			<h2 style={{
-				margin: "0px"
-			}}>Gluténmentes? {item.isGlutenFree ? "igen" : "nem"}</h2>
-			<img alt={item.name} title={item.name} src={item.kepURL} height="400" width="400"></img>
-		</div>
-	))
+		<Col>
+			<h1>{item.name}</h1>
+			<h2>Gluténmentes? {item.isGlutenFree ? "igen" : "nem"}</h2>
+			<Image alt={item.name} title={item.name} src={item.kepURL} style={{objectFit: "cover", width: 400, height: 400}} fluid />
+		</Col>
+	));
 
-    return items;
+    return [
+		<>
+		  {loaded ? (
+			<Container>
+			  <Row>{[...items]}</Row>
+			</Container>
+		  ) : (
+			<div className="d-flex flex-row min-vh-100 justify-content-center align-items-center">
+				<Spinner animation="border" role="status" />
+				<h1>Töltés...</h1>
+			</div>
+		  )}
+		</>
+	];
 }
 
 createRoot(document.getElementById("root")).render(<App />);
